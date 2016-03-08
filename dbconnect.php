a80d08a77e3f521d8fe8b4f42e9668272d85dc02
rimaaii <rgerhard@aii.edu>
2016-03-07 20:41:06 -0500

    Create dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,12 @@
+<?php 
+$link = mysqli_connect("localhost","root","");
+
+if (!$link)    
+{    
+ die('Could not connect: ' . mysqli_error());    
+}
+
+mysqli_select_db($link, "letsgolater");
+
+?> 
+
